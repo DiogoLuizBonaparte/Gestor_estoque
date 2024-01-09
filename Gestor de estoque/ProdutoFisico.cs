3416ca0f42e897d254377e86128137f5ccacb00b
Bonaparte <diogo322@hotmail.com>
2024-01-08 23:25:36 -0300

    Segundo commit

---
+++ Gestor de estoque/ProdutoFisico.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestor_de_estoque
+{
+    class ProdutoFisico : Produto, IEstoque
+    {
+        public float frete;
+        private   int estoque;
+
+        //Construtor
+
+        public ProdutoFisico(string nome, float preco, float frete)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.frete = frete;
+            
+        }
+
+        public void AdicionarEntrada()
+        {
+            
+        }
+
+        public void AdicionarSaida()
+        {
+           
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Frete: {frete}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Estoque: {estoque}");
+            Console.WriteLine("==========================");
+        }
+    }
+}
--- Gestor de estoque/Program.cs
+++ Gestor de estoque/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+
+class Program
+{
+    enum Menu { Listar = 1, Adicionar, Remover, Entrada, Saida, Sair}
+
+
+    public static void Main(string[] args)
+    {
+        bool escolheuSair = false;
+        while (escolheuSair == false)
+        {
+            Console.WriteLine("Controle de estoque");
+            Console.WriteLine("1 - Listar\n2 - Adicionar\n3 - Remover\n4 - Adicionar\n5 - Registrar entrada\n6 - Regisrar saida\n7 - Sair ");
+            int opInt = int.Parse(Console.ReadLine());
+
+            if (opInt < 1 && opInt > 7)
+            {
+
+                Menu escolha = (Menu)opInt;
+
+                switch (escolha)
+                {
+                    case Menu.Listar:
+                        break;
+                    case Menu.Adicionar:
+                        break;
+                    case Menu.Remover:
+                        break;
+                    case Menu.Entrada:
+                        break;
+                    case Menu.Saida:
+                        break;
+                    case Menu.Sair:
+                        escolheuSair |= true;
+                        break;
+
+                }
+            }
+            else
+            {
+                escolheuSair = true;
+            }
+
+        }
+
+    }
+
+    static void Cadastro()
+    {
+        Console.WriteLine("Cadastro de Produtos");
+        Console.WriteLine("1 - Produto Físic0\n2 - Ebook\n3 - Curso");
+
+        int opcao = int.Parse(Console.ReadLine());
+
+        switch(opcao) 
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+    static void CadastrarPFisico()
+    {
+         
+
+    }
+}
